38cd7f7399d115c0ea997c2bb3907c7e6b5077e7
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2020-10-14 21:36:20 +0530

    Adding comments for better understanding

--- front.py
+++ front.py
@@ -1,12 +1,12 @@
 from tkinter import *
-window =Tk()
-window.geometry("500x300")
-window.title("Attendence Monitering System")
-lb1 = Label(window, text = "Attendence Monitering", bg = "white", fg="black", font=20)
-lb1.place(x=150, y=10)
+window =Tk()  #creating root window from tk() constructor
+window.geometry("500x300") #setting the dimensions of window 
+window.title("Attendence Monitering System")  # setting the title 
+lb1 = Label(window, text = "Attendence Monitering", bg = "white", fg="black", font=20)  #inserting the label 
+lb1.place(x=150, y=10) # setting the cordinates of label 
 
 lb2_u = Label(window, text = "Student Name ", bg="white", fg="black", font=15)
-lb2_u2 = Entry(window,font=15)
+lb2_u2 = Entry(window,font=15) # entry is ysed to create the textbox 
 lb2_u.place(x=10, y=90)
 lb2_u2.place(x=180, y=90)
 
@@ -21,7 +21,5 @@ bt_Take_Images.place(x=80,y=210)
 bt_Train_Images = Button(window, text="Train Images",font=15,bg="green",fg="black")
 bt_Train_Images.place(x=270,y=210)
 
-
-
-
 window.mainloop()
+# setting an infinite loop to run the program which ends when the window is closed 
